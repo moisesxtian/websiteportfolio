1e8ab57396c2e1086d70157d6a1438f3021f3606
Christian <christiansmoises023@gmail.com>
2025-01-31 21:11:47 +0800

    Footer
    Added Simple Footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Projects from "./components/Projects";
 import Certificates from "./components/Certificates"
 import Experience from "./components/Experience"
 import Contact from "./components/Contact"
+import Footer from "./components/Footer"
 
 
 function App(){
@@ -16,6 +17,7 @@ function App(){
       <Certificates/>
       <Experience/>
       <Contact/>
+      <Footer/>
     </div>
     
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer: React.FC = () => {
+    return (
+      <footer className="w-full  text-white text-center py-4">
+        <p className="pt-40 text-secondary-color opacity-70">Made with ❤️ by Christian Moises 2025</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
